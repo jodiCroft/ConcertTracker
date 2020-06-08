2dd2a03e62a04794cf77a07d5f090a30d75225d6
xemnikk <xemnikk@gmail.com>
2020-06-07 21:27:55 -0500

    fixed weird error in code in Crowd class

--- crowd.rb
+++ crowd.rb
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
 class Crowd
 
-#attributes: size(int), median_age(int), energy_level(int), political_preference(string)
-
-#methods: 
-
-#get_louder adds points to energy level and artist energy level
-
-#get_softer subtracts points from energy level and artist energy level
-
-#leave_early substracts from energy level, attendance, and artist energy level
-
-end 
-=======
-#attributes: attendance(int), median_age(int), energy_level(int), political_preference(string)
-
-#methods: 
-
-#get_louder adds points to energy level and artist energy level
-
-#get_softer subtracts points from energy level and artist energy level
-
-#leave_early substracts from energy level, attendance, and artist energy level
->>>>>>> 7818b705981099443ed208980aab33838d425f18
+    #attributes: size(int), median_age(int), energy_level(int), political_preference(string)
+    
+    #methods: 
+    
+    #get_louder adds points to energy level and artist energy level
+    
+    #get_softer subtracts points from energy level and artist energy level
+    
+    #leave_early substracts from energy level, attendance, and artist energy level
+    
+    end 
