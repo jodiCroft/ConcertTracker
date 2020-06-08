7818b705981099443ed208980aab33838d425f18
smithspencer817 <smithspencer817@gmail.com>
2020-06-07 20:24:03 -0500

    added method suggestions to crowd.rb

--- crowd.rb
+++ crowd.rb
@@ -1,2 +1,9 @@
-#attributes: size(int), median_age(int), energy_level(int), political_preference(string)
-#methods: #get_louder, #get_softer, #leave_early
+#attributes: attendance(int), median_age(int), energy_level(int), political_preference(string)
+
+#methods: 
+
+#get_louder adds points to energy level and artist energy level
+
+#get_softer subtracts points from energy level and artist energy level
+
+#leave_early substracts from energy level, attendance, and artist energy level
